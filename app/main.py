7f6b476a02af1b0d71cf7467876c8a2c8d148ec3
bhaumik <bhaumik.yadav@argusoft.com>
2026-08-14 18:40:40 +0530

    FastAPI connection with Postgres DB with SQLAlchemy ORM

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -6,6 +6,11 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from . import models
+from sqlalchemy.orm import Session
+from .database import engine, SessionLocal, get_db
+
+models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
 
@@ -25,11 +30,19 @@ while True:
         print(e)
         time.sleep(2)
 
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {'Data':posts}
+
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts ORDER BY created_at DESC""")
-    posts = cursor.fetchall()
-    return {"data": posts}
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {'Data':posts}
+# def get_posts():
+    # cursor.execute("""SELECT * FROM posts ORDER BY created_at DESC""")
+    # posts = cursor.fetchall()
+    # return {"data": posts}
 
 @app.get("/posts/latest")
 def get_latest_post():
@@ -38,19 +51,27 @@ def get_latest_post():
     return {"last post": post}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"Created New Post": new_post}
+# def create_posts(post: Post):
+#     cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
+#     new_post = cursor.fetchone()
+#     conn.commit()
+#     return {"Created New Post": new_post}
 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s ORDER BY created_at DESC LIMIT 1""", (str(id),))
-    post = cursor.fetchone()
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    return {"post details": post}
+def get_post(id: int, post: Post, db: Session = Depends(get_db)):
+    new_post = db.query(models.Post).filter(models.Post.id == id).first()
+    return {"Created New Post": new_post}
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s ORDER BY created_at DESC LIMIT 1""", (str(id),))
+    # post = cursor.fetchone()
+    # if not post:
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    # return {"post details": post}
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
