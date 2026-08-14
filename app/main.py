1d16263db1cabf3d6110fb1643deac0fdb64690f
bhaumik <bhaumik.yadav@argusoft.com>
2026-08-14 15:57:35 +0530

    FastAPI connection with Postgres DB(No ORM)

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
 
@@ -12,56 +15,57 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[float] = None
 
-my_posts = [{"id": 1, "title": "title of post 1", "content": "content of post 1"}]
-
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
+while True:    
+    try:
+        conn = psycopg2.connect(host = 'localhost', database = 'fastapi', user = 'postgres', password = 'password123', cursor_factory = RealDictCursor)
+        cursor = conn.cursor()
+        print('DB connection successful')
+        break
+    except Exception as e:
+        print(e)
+        time.sleep(2)
 
 @app.get("/posts")
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts ORDER BY created_at DESC""")
+    posts = cursor.fetchall()
+    return {"data": posts}
 
 @app.get("/posts/latest")
 def get_latest_post():
-    id = len(my_posts) - 1
-    return my_posts[id]
+    cursor.execute("""SELECT * FROM posts ORDER BY created_at DESC LIMIT 1""")
+    post = cursor.fetchone()
+    return {"last post": post}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"Created New Post": new_post}
 
 @app.get("/posts/{id}")
 def get_post(id: int):
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s ORDER BY created_at DESC LIMIT 1""", (str(id),))
+    post = cursor.fetchone()
     if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"post with id: {id} was not found")
-    return {"post_detail": post}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    return {"post details": post}
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    a = find_index_post(id)
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
+    a = cursor.fetchone()
     if a == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id:{id} doesn\'t exist')
-    my_posts.pop(a)
+    conn.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {'Post Updated Successfully':post_dict}
+    conn.commit()
+    return {'Post Updated Successfully':updated_post}
