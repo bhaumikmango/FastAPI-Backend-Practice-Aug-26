7562725b1b3b45d3bb7cd9a5d550ed6c3d49e75f
bhaumik <bhaumik.yadav@argusoft.com>
2026-08-19 16:04:34 +0530

    Implement user authentication, database relationships with foreign keys, environment configuration, and vote routing with SQLAlchemy joins

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,7 +5,7 @@ from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(tags = ['Auth'])
 
-@router.post('/login')
+@router.post('/login', response_model = schemas.Token)
 def login(user_creds : OAuth2PasswordRequestForm = Depends(), db : Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == user_creds.username).first()
 
--- app/routers/post.py
+++ app/routers/post.py
@@ -2,22 +2,22 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from typing import List
 from ..database import get_db
-from .. import models, schemas, utils
+from .. import models, schemas, oauth2
 
 router = APIRouter(prefix='/posts', tags=['Posts'])
 
 @router.get("/", response_model = List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
     posts = db.query(models.Post).all()
     return posts
 
 @router.get("/latest", response_model=schemas.Post)
-def get_latest_post(db: Session = Depends(get_db)):
+def get_latest_post(db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).order_by(models.Post.created_at.desc()).first()
     return post
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model = schemas.Post)
-def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -25,14 +25,14 @@ def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
     return new_post
 
 @router.get("/{id}", response_model = schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_post(id: int, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     return post
     
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db : Session = Depends(get_db)):
+def delete_post(id: int, db : Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
     a = db.query(models.Post).filter(models.Post.id == id)
     if a.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id:{id} doesn\'t exist')
@@ -41,7 +41,7 @@ def delete_post(id: int, db : Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}", response_model = schemas.Post)
-def update_post(id: int, updated_post: schemas.Post, db : Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.PostCreate, db : Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
 
